ea4a8eb5a509b89ad3fa321620041923b6f5081c
Spenser Wellen <spenserwellen@gmail.com>
2021-08-20 10:07:11 -0500

    formatted crypto detail, after prettier was working again

--- screens/CryptoDetail.js
+++ screens/CryptoDetail.js
@@ -20,12 +20,12 @@ import { dummyData, COLORS, SIZES, FONTS, icons } from '../constants';
 import { HeaderBar, CurrencyLabel } from '../components';
 
 const CryptoDetail = ({ route, navigation }) => {
-const scrollX = new Animated.Value(0);
-const numberOfCharts = [1, 2, 3];
+	const scrollX = new Animated.Value(0);
+	const numberOfCharts = [1, 2, 3];
 
 	const [selectedCurrency, setSelectedCurrency] = useState(null);
-const [chartOptions, setChartOptions] = useState(dummyData.chartOptions);
-const [selectedOption, setSelectedOption] = useState(chartOptions[0]);
+	const [chartOptions, setChartOptions] = useState(dummyData.chartOptions);
+	const [selectedOption, setSelectedOption] = useState(chartOptions[0]);
 
 	useEffect(() => {
 		const { currency } = route.params;
@@ -66,52 +66,63 @@ const [selectedOption, setSelectedOption] = useState(chartOptions[0]);
 				</View>
 
 				{/* Chart */}
-				<Animated.ScrollView 
-				horizontal
-				pagingEnabled
-				scrollEventThrottle={16}
-				snapToAlignment='center'
-				snapToInterval={SIZES.width - 40}
-				showsHorizontalScrollIndicator={false}
-				decelerationRate={0}
-				onScroll={Animated.event([
-					{ nativeEvent: { contentOffset: { x: scrollX } } }
-				], { useNativeDriver: false })}
-				>
-				{
-					numberOfCharts.map((item, index) => (
-						<View key={`chart-${index}`} style={{marginLeft: index == 0 ? SIZES.base : 0}}>
-				<View style={{ marginTop: 25 }}>
-					<VictoryChart
-						theme={VictoryCustomTheme}
-						height={220}
-						width={SIZES.width - 40}>
-						<VictoryLine
-							style={{
-								data: {
-									stroke: COLORS.secondary,
-								},
-								parent: {
-									border: '1px solid #ccc',
-								},
-							}}
-							data={selectedCurrency?.chartData}
-							categories={{
-								x: ['15 MIN', '30 MIN', '45 MIN', '60 MIN'], y: ['15', '30', '45']
-							}}
-							/>
-							<VictoryScatter data={selectedCurrency?.chartData} size={7} style={{
-								data: {
-									fill: COLORS.secondary
-									}
-								}} />
-								<VictoryAxis style={{grid: {stroke: 'transparent'}}} />
-								<VictoryAxis dependentAxis style={{axis: {stroke: 'transparent'}, grid: {stroke: 'grey'}}} />
-					</VictoryChart>
-				</View>
-				</View>
-					))
-				}
+				<Animated.ScrollView
+					horizontal
+					pagingEnabled
+					scrollEventThrottle={16}
+					snapToAlignment='center'
+					snapToInterval={SIZES.width - 40}
+					showsHorizontalScrollIndicator={false}
+					decelerationRate={0}
+					onScroll={Animated.event(
+						[{ nativeEvent: { contentOffset: { x: scrollX } } }],
+						{ useNativeDriver: false }
+					)}>
+					{numberOfCharts.map((item, index) => (
+						<View
+							key={`chart-${index}`}
+							style={{ marginLeft: index == 0 ? SIZES.base : 0 }}>
+							<View style={{ marginTop: 25 }}>
+								<VictoryChart
+									theme={VictoryCustomTheme}
+									height={220}
+									width={SIZES.width - 40}>
+									<VictoryLine
+										style={{
+											data: {
+												stroke: COLORS.secondary,
+											},
+											parent: {
+												border: '1px solid #ccc',
+											},
+										}}
+										data={selectedCurrency?.chartData}
+										categories={{
+											x: ['15 MIN', '30 MIN', '45 MIN', '60 MIN'],
+											y: ['15', '30', '45'],
+										}}
+									/>
+									<VictoryScatter
+										data={selectedCurrency?.chartData}
+										size={7}
+										style={{
+											data: {
+												fill: COLORS.secondary,
+											},
+										}}
+									/>
+									<VictoryAxis style={{ grid: { stroke: 'transparent' } }} />
+									<VictoryAxis
+										dependentAxis
+										style={{
+											axis: { stroke: 'transparent' },
+											grid: { stroke: 'grey' },
+										}}
+									/>
+								</VictoryChart>
+							</View>
+						</View>
+					))}
 				</Animated.ScrollView>
 
 				{/* Options */}
